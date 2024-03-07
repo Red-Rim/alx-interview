3c03dc6a8701109c8e7b8e4ca5340b37c0d2d387
Red-Rim <salmaouakhir20@gmail.com>
2024-03-07 23:38:17 +0100

    my pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,9 +11,8 @@ def pascal_triangle(n):
 
         for i in range(n):
             row = [1] * (i + 1)
-            for j in range(1, i):
+            for j in range(1, len(row) - 1):
                 row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
             triangle.append(row)
 
         return triangle
-
