7bd16252a5c977449fc27e6b6b97bbfe9d4f1848
Red-Rim <salmaouakhir20@gmail.com>
2024-03-17 23:21:56 +0000

    red me

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""0. Lockboxes"""
-
+"""LOCKED"""
 
 def canUnlockAll(boxes):
     """
@@ -12,23 +11,18 @@ def canUnlockAll(boxes):
     Returns:
     - bool: True if all boxes can be unlocked, False otherwise.
     """
-    keys_collected = set()
-    boxes_count = len(boxes)
-    keys_count = 0
+    total_boxes = len(boxes)
+    keys = [0]
+    count = 0
+    queue = 0
 
-    for key in boxes[0]:
-        if key not in keys_collected and 0 < key < boxes_count:
-            keys_collected.add(key)
-            keys_count += 1
+    while queue < len(keys):
+        setkey = keys[queue]
+        for key in boxes[setkey]:
+            if 0 < key < total_boxes and key not in keys:
+                keys.append(key)
+                count += 1
+        queue += 1
 
-    index = 0
-    while index < len(keys_collected):
-        current_key = keys_collected.pop()
-        for key in boxes[current_key]:
-            if key not in keys_collected and 0 < key < boxes_count:
-                keys_collected.add(key)
-                keys_count += 1
-        index += 1
-
-    return keys_count == boxes_count - 1
+    return count == total_boxes - 1
 
