fd7b5469bd6d49bf0e206bd0ec10f28328de3762
Red-Rim <salmaouakhir20@gmail.com>
2024-03-22 01:20:42 +0000

    min op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,16 +3,27 @@
 
 
 def minOperations(n):
-    """ Minimum Operations """
+    """ Calculate the minimum number of operations needed to achieve n characters """
     if n <= 1:
         return 0
-    else:
-        i = 2
-        c = 0
-        while i <= n:
-            if n % i == 0:
-                c += i
-                n = n / i
-            else:
-                i += 1
-        return c
+    
+    i = 2
+    c = 0
+    while i <= n:
+        if n % i == 0:
+            c += i
+            n = n / i
+        else:
+            i += 1
+
+    if c == 0:
+        return 0
+
+    return c
+
+if __name__ == "__main__":
+    n = 4
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+    n = 12
+    print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
